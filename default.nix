460b61e1d9cb032e0c22f3f9ac94bca8edb819ba
MagicBytes <ppeinecke@protonmail.com>
2024-08-18 20:29:30 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,16 +2,16 @@
   pkgs ? import <nixpkgs> {},
 }:
 pkgs.stdenv.mkDerivation rec {
-  pname = "nixos-boot";
-  version = "0.1.0";
+  pname = "redflake-plymouth";
+  version = "0.0.1";
 
   src = ./src;
 
   installPhase = ''
-    mkdir -p $out/share/plymouth/themes/nixos-load
-    cd nixos-load
-    cp *png nixos-load.script nixos-load.plymouth $out/share/plymouth/themes/nixos-load
-    chmod +x $out/share/plymouth/themes/nixos-load/nixos-load.plymouth $out/share/plymouth/themes/nixos-load/nixos-load.script
-    sed -i "s@/usr/@$out/@" $out/share/plymouth/themes/nixos-load/nixos-load.plymouth
+    mkdir -p $out/share/plymouth/themes/redflake-plymouth
+    cd redflake-plymouth
+    cp *png redflake-plymouth.script redflake-plymouth.plymouth $out/share/plymouth/themes/redflake-plymouth
+    chmod +x $out/share/plymouth/themes/redflake-plymouth/redflake-plymouth.plymouth $out/share/plymouth/themes/redflake-plymouth/redflake-plymouth.script
+    sed -i "s@/usr/@$out/@" $out/share/plymouth/themes/redflake-plymouth/redflake-plymouth.plymouth
   '';
 }
